9bee5dd75fd413404ef77155fe0ca0119bb74d8b
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-30 14:21:24 +0530

    fect : Added dependency to app
    Retrive joke from libjoke and startactivity from jokeandroid

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,12 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.jokeandroid.JokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -42,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeActivity.class);
+        startActivity(intent);
     }
 
 
