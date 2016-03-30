bc0e944991e757e0771e24abb9d05a004d6cad3a
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-30 13:34:43 +0530

    fect : Added jokes to Joke class

--- libjoke/src/main/java/com/example/Joke.java
+++ libjoke/src/main/java/com/example/Joke.java
@@ -1,4 +1,12 @@
 package com.example;
 
 public class Joke {
+    public String getJoke() {
+        return "This is a lame joke";
+    }
+
+    public String goodJoke() {
+        return "A bear walks into a bar and says to the bartender, \"I'll have a pint of beer and a.......... packet of peanuts.\"\n" +
+                "The bartender asks, \"Why the big pause?\"";
+    }
 }
