17a2692b416c2b7ec8277e1aad3b0a0536c37918
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-04-01 12:22:25 +0530

    fect : Successfully running backend locally

--- backend/src/main/java/com/example/Aditya/myapplication/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/Aditya/myapplication/backend/MyEndpoint.java
@@ -6,29 +6,35 @@
 
 package com.example.Aditya.myapplication.backend;
 
+import com.example.Joke;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
 import javax.inject.Named;
 
-/** An endpoint class we are exposing */
+/**
+ * An endpoint class we are exposing
+ */
 @Api(
-  name = "myApi",
-  version = "v1",
-  namespace = @ApiNamespace(
-    ownerDomain = "backend.myapplication.Aditya.example.com",
-    ownerName = "backend.myapplication.Aditya.example.com",
-    packagePath=""
-  )
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.myapplication.Aditya.example.com",
+                ownerName = "backend.myapplication.Aditya.example.com",
+                packagePath = ""
+        )
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        String joke = Joke.getJoke();
+        response.setData(joke);
 
         return response;
     }
